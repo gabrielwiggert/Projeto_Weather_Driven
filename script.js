3d173ae882c11ccd97437752aa4867b6285e9a7d
Gabriel Wiggert <gabrielwiggert@gmail.com>
2022-03-04 20:09:04 -0300

    API request with user`s requested location successful

---
+++ script.js
@@ -0,0 +1,34 @@
+let startPos;
+let userLat;
+let userLon;
+
+let geoSuccess = function(position) {
+    alert("User located!");
+
+    startPos = position;
+    userLat = startPos.coords.latitude;
+    userLon = startPos.coords.longitude;
+
+    document.querySelector('body').innerHTML = startPos.coords.latitude + " " + startPos.coords.longitude;
+
+    requestAPI();
+};
+
+let geoError = function(error) {
+    switch(error.code) {
+        case error.TIMEOUT:
+        // The user didn't accept the callout
+        alert("Access to location denied! Cannot run application.");
+        break;
+    }
+};
+
+navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+
+function requestAPI() {
+    let promise = axios.post(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${userLat}&lon=${userLon}&appid=f25110b0f83adb9f7c080ee182cd1d00&units=metric`);
+
+    promise.then(success => alert("Request successful!"));
+    promise.catch(error => alert('Request failed'));
+}
